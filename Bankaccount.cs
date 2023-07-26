9dc520cd0c1598deacd92175a159eb8984c55eb1
charan1805 <sreecharanmudhiraj@gmail.com>
2023-07-26 15:09:06 +0530

    First Class

---
+++ Bankaccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assesment_16
+{
+  
+        internal class BankAccount
+        {
+
+
+            private static int accountCounter = 1000;
+            public int AccountNumber { get; }
+            public string AccountHolderName { get; }
+            private double Balance { get; set; }
+
+            public BankAccount(string accountHolderName)
+            {
+                AccountNumber = GenerateAccountNumber();
+                AccountHolderName = accountHolderName;
+                Balance = 0;
+            }
+
+            private static int GenerateAccountNumber()
+            {
+                return ++accountCounter;
+            }
+
+            public void Deposit(double amount)
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentException("Deposit amount must be greater than zero.");
+                }
+
+                Balance += amount;
+            }
+            public void Withdraw(double amount)
+            {
+                if (amount <= 0)
+                {
+                    throw new ArgumentException("Withdrawal amount must be greater than zero.");
+                }
+
+                if (amount > Balance)
+                {
+                    throw new InvalidOperationException("Insufficient funds.");
+                }
+
+                Balance -= amount;
+            }
+            public double GetBalance()
+            {
+                return Balance;
+            }
+        }
+    }
+
+
